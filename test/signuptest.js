6af867ab3bf76826226bbeebac9684f6aac46594
frandosis <frandosis@hotmail.com>
2021-04-29 15:18:13 +0200

    added test scripts

---
+++ test/signuptest.js
@@ -0,0 +1,44 @@
+const io = require("socket.io-client");
+
+var socket = io.connect("http://172.26.127.94:3000");
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  } 
+  socket.on("connect", () => {
+      test();
+    });
+  socket.on("login", (res)=>{
+      console.log(res);
+  })
+  
+let validuser = {
+    email: "pingvin@linux.com",
+    username: "Bubber",
+    hashed_password: "Bubber",
+}
+
+
+let invaliduser = {
+    email: "pingvin@localhost",
+    username: "",
+    hashed_password: "UMOM",
+}
+
+let notuser = {
+    date: "19/12/2020",
+    age: 21,
+    essential: true,
+}
+
+async function test (){
+    await sleep(1000)
+    socket.emit("login", validuser);
+    await sleep(1000)
+    socket.emit("login", invaliduser);
+    await sleep(1000)
+    socket.emit("login", notuser);
+    await sleep(1000)
+    socket.disconnect();
+    }
