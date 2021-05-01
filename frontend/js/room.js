6b1c1c672b86487992ec62c41468dd9585d75191
Cloud server <cloud@cloud.cloud>
2021-05-01 14:56:42 +0200

    room changes

--- frontend/js/room.js
+++ frontend/js/room.js
@@ -1,3 +1,4 @@
+var socket = io.connect("http://130.225.170.76/room.html");
 
 var name = getQueryVariable("name") || "Anonymous";
 var greetings = "Welcome " + localStorage.getItem("cookieID") + "!";
@@ -5,7 +6,17 @@ document.getElementById("header-text").innerHTML = greetings;
 console.log(name);
 console.log("Local storage: ", localStorage.getItem("cookieID"));
 
+socket.on("getallroms", (res) => {
+    console.log("Respond: ", res);
+});
+
 function redirectToLogin() {
     localStorage.removeItem("cookieID");
     window.location.replace("/login.html");
+}
+
+function searchRooms(){
+    //socket.emit("getallrooms");
+    let rooms = document.getElementById("rooms");
+    console.log("Rooms: ", rooms);
 }
