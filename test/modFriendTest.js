dbe3e12c1ae5e581cc97b142371c9344e856fac7
Cloud server <cloud@cloud.cloud>
2021-05-08 10:49:24 +0200

    test

---
+++ test/modFriendTest.js
@@ -0,0 +1,77 @@
+const io = require("socket.io-client");
+var socket = io.connect("http://localhost:3005");
+var answer;
+
+function sleep(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+socket.on("connect", () => {
+	connect();
+});
+
+socket.on("signup", (res) => {
+	console.log(res);
+});
+
+socket.on("login", (res) => {
+	console.log(res);
+	answer = res;
+	testFriend();
+});
+
+socket.on("modFriend", (res) => {
+	console.log(res);
+});
+
+let validuser0 = {
+	// Success true; only the first time ;)
+	email: "pingvin@linux.com",
+	username: "Bubber",
+	hashed_password: "Bubber",
+};
+
+let validuser1 = {
+	// Success true; only the first time ;)
+	email: "isbjørn@linux.com",
+	username: "Anders",
+	hashed_password: "Anders",
+};
+
+let loginUser0 = {
+	email: "pingvin@linux.com",
+	hashed_password: "Bubber",
+};
+
+function connect() {
+	//await sleep(1000);
+	socket.emit("signin", validuser0);
+	//await sleep(1000);
+	socket.emit("signin", validuser1);
+	//await sleep(1000);
+	socket.emit("login", loginUser0);
+	//await sleep(1000);
+}
+
+function testFriend() {
+	let addFriend = {
+		userID: answer.user._id,
+		friendName: "Anders",
+		addFriend: true,
+	};
+
+	let removeFriend = {
+		userID: answer.user._id,
+		friendName: "Anders",
+		addFriend: false,
+	};
+
+	socket.emit("modFriend", addFriend);
+	//await sleep(1000);
+	socket.emit("modFriend", removeFriend);
+	//await sleep(1000);
+
+	socket.disconnect();
+}
